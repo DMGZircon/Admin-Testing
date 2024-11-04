4667b17481ee14f4c083b57e7b88b18901987cb7
Leonard Sinining <lnardsing120702@gmail.com>
2024-11-04 13:59:10 +0800

    Admin Testing

--- src/components/Admin.tsx
+++ src/components/Admin.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Box, Button, TextField } from '@mui/material';
 import { Bar } from 'react-chartjs-2';
